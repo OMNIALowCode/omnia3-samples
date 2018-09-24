3673f08f4dfbb58fcf7afba2dadbe235e987f2d6
Nuno Ferreira <nuno.ferreira@mymis365.onmicrosoft.com>
2018-09-24 09:10:12 +0100

    Add select component

--- webcomponents/select/omnia-select.js
+++ webcomponents/select/omnia-select.js
@@ -1,35 +1,66 @@
-class TextareaElement extends HTMLElement {
-
-  constructor() {
-    super();
-
-    this.textarea = document.createElement('textarea');
-    this.textarea.setAttribute('class', 'form-control');
-
-    this.textarea.onchange = this.valueUpdated.bind(this);
-  }
-
-  connectedCallback() {
-    this.appendChild(this.textarea);
-  }
-
-  valueUpdated(event) {
-    // When the onChange event is fired, dispatch a new event 'value-updated' to notify the OMNIA Platform that the values has been updated
-    this.dispatchEvent(new CustomEvent('value-updated',
-      {
-        detail: {
-          value: event.target.value
-        }
-      }));
-  }
-
-  set value(newValue) {
-    this.textarea.value = newValue;
-  }
-
-  set isReadOnly(newValue) {
-    this.textarea.disabled = (newValue === true);
-  }
+class OmniaSelect extends HTMLElement {
+	constructor() {
+		super();
+	
+		this._options = [{ value: '', text: 'Please select a value'}];
+		this.select = document.createElement('select');
+	}
+  
+	connectedCallback() {
+	    this.renderOptions();
+		
+		this.select.selectedIndex = 0;
+		this.select.addEventListener('change', this.valueUpdated.bind(this));
+		this.select.setAttribute('class', 'form-control');
+		
+		this.appendChild(this.select);
+	}
+	
+	disconnectedCallback() {
+		this.select.removeEventListener('change', this.valueUpdated.bind(this));
+	}
+	
+	valueUpdated(event) {
+		let evtValue = (event.target.value || event.target.options[event.target.selectedIndex].value);
+		
+		this.dispatchEvent(new CustomEvent('value-updated', {
+			detail: {
+				value: evtValue
+			}
+		}));
+	}
+	
+	
+	renderOptions() {
+		this.select.innerHTML = '';
+	    
+		this._options.forEach(opt => {
+            let option = document.createElement('option');
+            option.value = opt.value;
+            option.text = opt.text;
+ 
+            this.select.appendChild(option);
+        });
+	}
+  
+    set value(newValue) {
+        const newValueIndex = this._options.map(obj => obj.value).indexOf(newValue);
+		this.select.selectedIndex = newValueIndex >= 0 ? newValueIndex : 0;
+    }
+    
+    get options() {
+        return this._options;
+    }
+	
+	set options(newValue) {
+		this._options = this._options.concat(newValue);
+		this.renderOptions();
+	}
+	
+	set isReadOnly(newValue)
+    {
+        this.select.disabled = (newValue === true);
+    }
 }
 
-customElements.define('omnia-textarea', TextareaElement);
+customElements.define('omnia-select', OmniaSelect);
