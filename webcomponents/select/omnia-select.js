2b2ec5a411c2b94c21e5251d8192623908458958
Nuno Ferreira <nuno.ferreira@mymis365.onmicrosoft.com>
2018-09-24 09:09:12 +0100

    Add select box

---
+++ webcomponents/select/omnia-select.js
@@ -0,0 +1,35 @@
+class TextareaElement extends HTMLElement {
+
+  constructor() {
+    super();
+
+    this.textarea = document.createElement('textarea');
+    this.textarea.setAttribute('class', 'form-control');
+
+    this.textarea.onchange = this.valueUpdated.bind(this);
+  }
+
+  connectedCallback() {
+    this.appendChild(this.textarea);
+  }
+
+  valueUpdated(event) {
+    // When the onChange event is fired, dispatch a new event 'value-updated' to notify the OMNIA Platform that the values has been updated
+    this.dispatchEvent(new CustomEvent('value-updated',
+      {
+        detail: {
+          value: event.target.value
+        }
+      }));
+  }
+
+  set value(newValue) {
+    this.textarea.value = newValue;
+  }
+
+  set isReadOnly(newValue) {
+    this.textarea.disabled = (newValue === true);
+  }
+}
+
+customElements.define('omnia-textarea', TextareaElement);
