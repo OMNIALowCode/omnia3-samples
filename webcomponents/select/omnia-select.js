83ead4c8020a9b67529ea6d1d5cfa31c68718bbd
Nuno Ferreira <nuno.ferreira@mymis365.onmicrosoft.com>
2018-12-14 10:44:06 +0000

    Add translations to omnia-select

--- webcomponents/select/omnia-select.js
+++ webcomponents/select/omnia-select.js
@@ -1,51 +1,72 @@
+// The component only supports en-us and pt-pt languages
+const TRANSLATIONS = {
+    default: {
+        DefaultOption: 'Please select a value',
+    },
+    ptpt: {
+        DefaultOption: 'Selecione uma opção',
+    }
+};
+
+function getTranslation(translation, language) {
+    const translationSet = language != null ? TRANSLATIONS[language.toLowerCase()] || TRANSLATIONS.default : TRANSLATIONS.default;
+    return translationSet ? translationSet[translation] : translation;
+}
+
+function getDefaultOption(translator){
+	return { value: '', text: getTranslation('DefaultOption',  translator ? translator.language : null) };
+}
+
+function getSelectOption(opt, translator){
+	const option = document.createElement('option');
+	option.value = opt.value;
+	option.text = translator ? translator.translateToApplication(opt.text) : opt.text;
+	return option;
+}
+
 class OmniaSelect extends HTMLElement {
 	constructor() {
 		super();
 	
-		this._options = [{ value: '', text: 'Please select a value'}];
-		this.select = document.createElement('select');
+		this._options = [];
+
+		this.valueUpdated = this.valueUpdated.bind(this);
+
+		this._select = document.createElement('select');
+		this._select.setAttribute('class', 'form-control');
+		this._select.addEventListener('change', this.valueUpdated);
 	}
   
 	connectedCallback() {
-	    this.renderOptions();
+		this.renderOptions();
+		this._select.selectedIndex = 0;
 		
-		this.select.selectedIndex = 0;
-		this.select.addEventListener('change', this.valueUpdated.bind(this));
-		this.select.setAttribute('class', 'form-control');
-		
-		this.appendChild(this.select);
+		this.appendChild(this._select);
 	}
 	
 	disconnectedCallback() {
-		this.select.removeEventListener('change', this.valueUpdated.bind(this));
+		this._select.removeEventListener('change', this.valueUpdated);
 	}
 	
-	valueUpdated(event) {
-		let evtValue = (event.target.value || event.target.options[event.target.selectedIndex].value);
+	valueUpdated() {
+		const currentSelection = this._select.selectedIndex;
+		this.dispatchEvent(new CustomEvent('value-updated', { detail: { value: currentSelection > 0 ? this._options[currentSelection - 1].value : '' } }));
+	}
+	
+	renderOptions(translator) {
+		const currentSelection = this._select.selectedIndex;
+		this._select.innerHTML = '';
 		
-		this.dispatchEvent(new CustomEvent('value-updated', {
-			detail: {
-				value: evtValue
-			}
-		}));
-	}
-	
-	
-	renderOptions() {
-		this.select.innerHTML = '';
-	    
-		this._options.forEach(opt => {
-            let option = document.createElement('option');
-            option.value = opt.value;
-            option.text = opt.text;
- 
-            this.select.appendChild(option);
-        });
+		this._select.append(getSelectOption(getDefaultOption(translator)));
+		for(const option of this._options)
+			this._select.appendChild(getSelectOption(option, translator));
+
+		this._select.selectedIndex = currentSelection;
 	}
   
     set value(newValue) {
-        const newValueIndex = this._options.map(obj => obj.value).indexOf(newValue);
-		this.select.selectedIndex = newValueIndex >= 0 ? newValueIndex : 0;
+        const newValueIndex = this._options.map(obj => obj.value).indexOf(newValue) + 1;
+		this._select.selectedIndex = newValueIndex >= 0 ? newValueIndex : 0;
     }
     
     get options() {
@@ -53,13 +74,19 @@ class OmniaSelect extends HTMLElement {
     }
 	
 	set options(newValue) {
-		this._options = this._options.concat(newValue);
+		this._options = newValue;
 		this.renderOptions();
 	}
 	
 	set isReadOnly(newValue)
     {
-        this.select.disabled = (newValue === true);
+        this._select.disabled = (newValue === true);
+	}
+	
+	set context(newValue) {
+        if (newValue) {
+			this.renderOptions(newValue.getLanguageTranslator());
+        }
     }
 }
 
