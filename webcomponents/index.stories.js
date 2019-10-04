e02593afbeadc8bc7997b51f572beefe0ea55d2d
Nuno Ferreira <nuno.ferreira@numbersbelieve.com>
2019-10-04 17:39:21 +0100

    Update initial page

--- webcomponents/index.stories.js
+++ webcomponents/index.stories.js
@@ -4,5 +4,16 @@ import { storiesOf } from '@storybook/html';
 storiesOf('Introduction', module)
     .addParameters({ info: { disable: true } })
     .add('About', () => {
-        return `<h1>OMNIA Platform Web Components</h1>`;
+        return `
+            <h1>OMNIA Platform</h1>
+            <h3>Welcome to our Web Component playground!</h3>
+            <div>
+                <br />
+                <p>Test all available web components.</p>
+                <p>Get creative with configurations.</p>
+                <p>Grab the source code and add them to your applications!</p>
+                <br />
+                <p>If you have any questions or need any sort of assistance, feel free to email us at <a href="mailto:support@numbersbelieve.com">support@numbersbelieve.com</a>.</p>
+            </div>
+        `;
     });
