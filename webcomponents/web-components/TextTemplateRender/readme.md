62620ef471124f407bc8af2a4e0403d153b39b4c
Mário Vaz <mario.vaz@mymis365.onmicrosoft.com>
2019-12-17 15:25:00 +0000

    Implemented text template render component

---
+++ webcomponents/web-components/TextTemplateRender/readme.md
@@ -0,0 +1,21 @@
+[View source code](https://github.com/OMNIALowCode/omnia3-samples/blob/master/webcomponents/web-components/TextTemplateRender/text-template-render.js)
+
+Description
+
+### Customization
+| Property | Description                     | Default Value |
+|----------|---------------------------------|---------------|
+| identifier | Sets the text template name |          |
+| parameters | Object representing text template parameters (see example below) | {}        |
+
+**Example**
+
+{
+    
+    parameter1: "parameter1 value",
+
+    parameter2: "parameter2 value",
+
+    ...
+
+}
---
+++ webcomponents/web-components/TextTemplateRender/text-template-render.js
@@ -0,0 +1,45 @@
+class TextTemplateRenderElement extends HTMLElement {
+
+    constructor() {
+        super();
+
+        this.container = document.createElement('div');
+
+        this._settings = {identifier: null, parameters: null};
+        this._context = null;
+    }
+
+    connectedCallback() {
+        this.appendChild(this.container);
+    }
+
+    set identifier(newValue) {
+        this._settings.identifier = newValue;
+        this.updateTemplate();
+    }
+
+    set parameters(newValue) {
+        this._settings.parameters = newValue;
+        this.updateTemplate();
+    }
+
+    set context(newValue) {
+        this._context = newValue;
+    }
+
+    updateTemplate(){
+        if(this._context == null || this._settings.identifier == null)
+            return;
+            
+        const apiClient = this._context.createApiHttpClient();
+
+        apiClient
+            .doPost(`/api/v1/${this._context.tenant.code}/${this._context.tenant.environmentCode}/application/TextTemplates/${this._settings.identifier}`, this._settings.parameters || {})
+            .then(response => response.data.text())
+            .then(text => {
+                this.container.innerHTML = text;
+            });
+    }
+}
+
+customElements.define('omnia-text-template-render', TextTemplateRenderElement);
