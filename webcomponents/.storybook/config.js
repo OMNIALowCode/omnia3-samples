05ea6aa216b862fd1673a2f7964532dc09b870a3
Nuno Ferreira <nuno.ferreira@numbersbelieve.com>
2019-12-18 10:51:13 +0000

    Update packages

--- webcomponents/.storybook/config.js
+++ webcomponents/.storybook/config.js
@@ -1,6 +1,7 @@
 import { configure, addDecorator, addParameters } from '@storybook/html';
 import 'bootstrap/dist/css/bootstrap.css';
 import { withKnobs } from '@storybook/addon-knobs';
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../web-components', true, /\.stories\.js$/);
@@ -11,6 +12,10 @@ function loadStories() {
 }
 
 addDecorator(withKnobs);
-addParameters({ viewport: {} });
+addParameters({
+    viewport: {
+        viewports: INITIAL_VIEWPORTS,
+    },
+});
 
 configure(loadStories, module);
