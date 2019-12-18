94caf1fdaac4299d5b5612e46b8b19a49ed9e842
Nuno Ferreira <nuno.ferreira@numbersbelieve.com>
2019-12-18 12:46:38 +0000

    Add addons-docs

--- webcomponents/.storybook/config.js
+++ webcomponents/.storybook/config.js
@@ -3,19 +3,14 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { withKnobs } from '@storybook/addon-knobs';
 import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
 
-// automatically import all files ending in *.stories.js
-const req = require.context('../web-components', true, /\.stories\.js$/);
-
-function loadStories() {
-    require('../index.stories');
-    req.keys().forEach(filename => req(filename));
-}
-
 addDecorator(withKnobs);
 addParameters({
+    options: {
+        storySort: (a, b) => a[1].id.localeCompare(b[1].id)
+    },
     viewport: {
         viewports: INITIAL_VIEWPORTS,
     },
 });
 
-configure(loadStories, module);
+configure(require.context('../web-components', true, /\.stories\.(js|mdx)$/), module);
