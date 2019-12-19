f03c5924d2d8fade5f8d254c01b478a18a4c35f7
Guilherme Ferreira <guilherme.as.ferreira@gmail.com>
2019-12-19 16:44:14 +0000

    Build storybook

--- webcomponents/web-components/Santa/santa.stories.js
+++ webcomponents/web-components/Santa/santa.stories.js
@@ -5,13 +5,16 @@ import { storiesOf } from '@storybook/html';
 import './santa';
 import readme from './readme.md';
 
-storiesOf('MerryChristmas', module)
-    .add('default', () => {
-        const component = createElement();
-        return component;
-    }, { notes: readme });
+storiesOf('Merry Christmas', module).add(
+  'default',
+  () => {
+    const component = createElement();
+    return component;
+  },
+  { notes: readme }
+);
 
 function createElement() {
-    const element = document.createElement('omnia-santa');
-    return element;
+  const element = document.createElement('omnia-santa');
+  return element;
 }
