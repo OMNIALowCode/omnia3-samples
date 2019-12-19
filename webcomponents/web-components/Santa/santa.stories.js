f2522aaff704f66a23bdddf7118fede043f1c19d
daniel8lopes <daniel8lopes@gmail.com>
2019-12-19 12:47:46 +0000

    Update santa.stories.js

--- webcomponents/web-components/Santa/santa.stories.js
+++ webcomponents/web-components/Santa/santa.stories.js
@@ -5,16 +5,11 @@ import { storiesOf } from '@storybook/html';
 import './santa';
 import readme from './readme.md';
 
-storiesOf('Data Input|Select box', module)
+storiesOf('MerryChristmas', module)
     .add('default', () => {
         const component = createElement();
-
-     
-
         return component;
-
     }, { notes: readme });
-
 
 function createElement() {
     const element = document.createElement('omnia-santa');
