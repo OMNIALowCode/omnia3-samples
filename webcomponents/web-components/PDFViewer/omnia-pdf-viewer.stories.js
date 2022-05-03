49aeed35dcad877d6d80d6cf3cf9bd7ff710e42f
OMNIALowCode <support@numbersbelieve.com>
2022-05-03 13:16:58 +0000

    Automated Release 67

---
+++ webcomponents/web-components/PDFViewer/omnia-pdf-viewer.stories.js
@@ -0,0 +1,30 @@
+import { document, console } from 'global';
+import './omnia-pdf-viewer';
+import { object, text } from '@storybook/addon-knobs';
+import mdx from './omnia-pdf-viewer.mdx';
+
+export default {
+  name: 'OMNIAPDFViewerElement',
+  title: 'Visualization/PDF Viewer',
+  parameters: {
+    docs: {
+      page: mdx,
+    },
+  },
+};
+
+export const Default = () => {
+  const element = createElement();
+  element.value = text('Value', 'https://uploads.codesandbox.io/uploads/user/ffc61027-3187-4e36-aaf9-d3d2b0e8d39e/qHb8-example.pdf');
+
+  return element;
+};
+
+Default.story = {
+  name: 'default',
+  parameters: { notes: readme },
+};
+
+function createElement() {
+  return document.createElement('omnia-pdf-viewer');
+}
