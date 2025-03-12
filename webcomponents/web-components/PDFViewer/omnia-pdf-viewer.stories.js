a7e7fdae9248f234cf411c462c7788c4999c132c
Diogo Teixeira <deefilipe@gmail.com>
2025-03-12 13:01:51 +0000

    Fixed upgrade issues

--- webcomponents/web-components/PDFViewer/omnia-pdf-viewer.stories.js
+++ webcomponents/web-components/PDFViewer/omnia-pdf-viewer.stories.js
@@ -1,6 +1,6 @@
-import { document, console } from 'global';
+import { text } from '@storybook/addon-knobs';
+import { document } from 'global';
 import './omnia-pdf-viewer';
-import { object, text } from '@storybook/addon-knobs';
 import mdx from './omnia-pdf-viewer.mdx';
 
 export default {
@@ -15,14 +15,16 @@ export default {
 
 export const Default = () => {
   const element = createElement();
-  element.value = text('Value', 'https://uploads.codesandbox.io/uploads/user/ffc61027-3187-4e36-aaf9-d3d2b0e8d39e/qHb8-example.pdf');
+  element.value = text(
+    'Value',
+    'https://uploads.codesandbox.io/uploads/user/ffc61027-3187-4e36-aaf9-d3d2b0e8d39e/qHb8-example.pdf',
+  );
 
   return element;
 };
 
 Default.story = {
   name: 'default',
-  parameters: { notes: readme },
 };
 
 function createElement() {
